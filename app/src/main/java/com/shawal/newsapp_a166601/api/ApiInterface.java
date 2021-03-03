83aaceeadcb0288327561bdee2b76e16bdd45342
mohdnoorshawal <mohdnoorshawal@ymail.com>
2021-03-03 18:59:30 +0800

    Done this project

---
+++ app/src/main/java/com/shawal/newsapp_a166601/api/ApiInterface.java
@@ -0,0 +1,29 @@
+package com.shawal.newsapp_a166601.api;
+
+import com.shawal.newsapp_a166601.models.News;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Query;
+
+public interface ApiInterface {
+
+    @GET("top-headlines?country=my")
+    Call<News> getNews(
+
+            @Query("country") String country,
+            @Query("apiKey") String apiKey
+
+    );
+
+    @GET("everything")
+    Call<News> getNewsSearch(
+
+            @Query("q") String keyword,
+            @Query("language") String language,
+            @Query("sortBy") String sortBy,
+            @Query("apiKey") String apiKey
+
+    );
+
+}
